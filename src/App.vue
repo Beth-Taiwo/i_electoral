4189d18ff3dd8224abbb6669484345d595be514f
Beth-Tee23 <popoolataiwo22@gmail.com>
2020-10-22 17:08:45 +0000

    updated sidebar and 'WIP' add election modal

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,24 @@
 * {
     font-family: 'Source Sans Pro', sans-serif;
 }
+
+#app {
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+}
+
+h3 {
+    margin: 18px;
+}
+
+.heading {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.title {
+    border-bottom: 1px solid rgb(0 0 0 / 10%);
+}
 </style>
